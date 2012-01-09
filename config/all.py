a02a0eeecc288b72d80609c9f18b34f5a918a326
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2012-01-08 23:38:19 -0430

    Generalizing the book version

--- config/all.py
+++ config/all.py
@@ -36,10 +36,10 @@ copyright = u'2011, Cake Software Foundation, Inc'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.x'
 
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0'
+release = '2.x'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
