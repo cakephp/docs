19e35de88fd2bc2663561a2b62b6b78410a35289
mark_story <mark@mark-story.com>
2012-02-19 13:29:48 -0500

    Styling things.
    - admonitions
    - glossary
    - index
    - rst things

--- config/all.py
+++ config/all.py
@@ -94,6 +94,7 @@ html_theme_path = ['../themes']
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
+html_short_title = u'Cookbook 2.x'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
