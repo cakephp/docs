34c9ed6c4a530f76baf943ee5e9fea218b7f3b1c
mark_story <mark@mark-story.com>
2012-02-20 21:43:27 -0500

    Sexy search.

--- themes/cakephp/static/app.js
+++ themes/cakephp/static/app.js
@@ -2,55 +2,60 @@ App = {};
  
 App.Book = (function() {
  
-    var menu = [];
+	var menu = [];
 	var base;
 	var delay = (function(){
 		var timer = 0;
 		return function(callback, ms){
-			clearTimeout (timer);
+			clearTimeout(timer);
 			timer = setTimeout(callback, ms);
 		};
 	})();
 
-    function init() {
+	var positionElement = function (to, element) {
+		var position = to.offset();
+		var height = to.outerHeight();
+		var width = to.outerWidth();
+
+		element.css({
+			position: 'absolute',
+			top: position.top + height + 'px',
+			left: position.left + 'px',
+			width: width + 'px'
+		});
+	};
+
+	function init() {
 		base = location.href.replace(location.protocol + '//' + location.host, '').split('/').slice(0, 3).join('/') + '/'; 
-        // SEARCH EVENT
-        $('#searchform input.search-input').keyup(function() {
-            if ($(this).val() === '') {
-                $('#search-results ul').fadeOut(150, function() {
-                    $(this).empty();
-                    $('ul.current').fadeIn(150);
-                });
-            } else {
-				var self = this;
+		var searchResults = $('#search-results');
+
+		// SEARCH EVENT
+		$('.search-input').keyup(function() {
+			if ($(this).val() === '') {
+				searchResults.fadeOut('fast');
+			} else {
+				var _this = $(this);
 				delay(function() {
-					$('ul.current').fadeOut(150);
-					$('#search-results ul').show();
-					search_menu($(self).val());
+					positionElement(_this, searchResults);
+					searchResults.show();
+					search_menu(_this.val());
 				}, 200);
 			}
+		});
 
-        });
 
+		$(document).keyup(function(e) {
+			// escape key
+			if (e.keyCode == 27) {
+				if ($('.search-input').val() === '') {
+					searchResults.fadeOut('fast');
+				}
+			}
+		});
 
-        $(document).keyup(
-                function(e) {
-                    // escape key
-                    if (e.keyCode == 27) {
-                        if ($('#searchform input.search-input').val() === '') {
-							$('#search-results ul').fadeOut(150, function() {
-			                    $(this).empty();
-						        $('ul.current').fadeIn(150);
-							});
-                        }
-                    }
-        });
-
-        $("p.searchtip").after("<div id='search-results'><ul></ul></div>");
-
-        $.getJSON(base + '_static/menu.json', function(data) {
+		$.getJSON(base + '_static/menu.json', function(data) {
 			menu = data;
-        });
+		});
 
 		// Make top nav responsive.
 		$('#cakephp-global-navigation ul').responsiveMenu();
@@ -68,6 +73,7 @@ App.Book = (function() {
 			});
 		});
 
+		// Display page contents
 		$('#page-contents-button').bind('click', function (e) {
 			e.preventDefault();
 			e.stopPropagation();
@@ -82,102 +88,102 @@ App.Book = (function() {
 
 	}
 
-    function search_menu(query) {
-        $('#search-results ul').empty();
-        var matches = [];
-        $.each(menu, function(index, item) {
-            var scored_node = {
-                score : item.text.toLowerCase().score(query.toLowerCase()),
-                node : item
-            };
-            matches.push(scored_node);
-        });
-        matches.sort(compare_scores);
-        results = matches.slice(0, 20);
-        $.each(results, function(index, item) {
-            if (item.score > 0) {
-                $('#search-results ul').append(
+	function search_menu(query) {
+		$('#search-results').empty().append('<ul></ul>');
+		var matches = [];
+		$.each(menu, function(index, item) {
+			var scored_node = {
+				score : item.text.toLowerCase().score(query.toLowerCase()),
+				node : item
+			};
+			matches.push(scored_node);
+		});
+		matches.sort(compare_scores);
+		results = matches.slice(0, 20);
+		$.each(results, function(index, item) {
+			if (item.score > 0) {
+				$('#search-results ul').append(
 					"<li><a href='" + base + item.node.link + "'>" + 
 					item.node.text + "</a></li>");
-            }
-        });
-    }
+			}
+		});
+	}
  
-    function compare_scores(a, b) {
-        return b.score - a.score;
-    }
+	function compare_scores(a, b) {
+		return b.score - a.score;
+	}
  
-    return {
-        init : init
-    }
+	return {
+		init : init
+	}
 })();
 
 
 function levenshtein (s1, s2) {
-      // http://kevin.vanzonneveld.net
-      // +            original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
-      // +            bugfixed by: Onno Marsman
-      // +             revised by: Andrea Giammarchi (http://webreflection.blogspot.com)
-      // + reimplemented by: Brett Zamir (http://brett-zamir.me)
-      // + reimplemented by: Alexander M Beedie
-      // *                example 1: levenshtein('Kevin van Zonneveld', 'Kevin van Sommeveld');
-      // *                returns 1: 3
+	  // http://kevin.vanzonneveld.net
+	  // +			  original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
+	  // +			  bugfixed by: Onno Marsman
+	  // +			   revised by: Andrea Giammarchi (http://webreflection.blogspot.com)
+	  // + reimplemented by: Brett Zamir (http://brett-zamir.me)
+	  // + reimplemented by: Alexander M Beedie
+	  // *				  example 1: levenshtein('Kevin van Zonneveld', 'Kevin van Sommeveld');
+	  // *				  returns 1: 3
 
-    if (s1 == s2) {
-        return 0;
-    }
+	if (s1 == s2) {
+		return 0;
+	}
 
-    var s1_len = s1.length;
-    var s2_len = s2.length;
-    if (s1_len === 0) {
-        return s2_len;
-    }
-    if (s2_len === 0) {
-        return s1_len;
-    }
+	var s1_len = s1.length;
+	var s2_len = s2.length;
+	if (s1_len === 0) {
+		return s2_len;
+	}
+	if (s2_len === 0) {
+		return s1_len;
+	}
 
-    // BEGIN STATIC
-    var split = false;
-    try{
-        split=!('0')[0];
-    } catch (e){
-        split=true; // Earlier IE may not support access by string index
-    }
-    // END STATIC
-    if (split){
-        s1 = s1.split('');
-        s2 = s2.split('');
-    }
+	// BEGIN STATIC
+	var split = false;
+	try{
+		split=!('0')[0];
+	} catch (e){
+		split=true; // Earlier IE may not support access by string index
+	}
+	// END STATIC
+	if (split){
+		s1 = s1.split('');
+		s2 = s2.split('');
+	}
 
-    var v0 = new Array(s1_len+1);
-    var v1 = new Array(s1_len+1);
+	var v0 = new Array(s1_len+1);
+	var v1 = new Array(s1_len+1);
 
-    var s1_idx=0, s2_idx=0, cost=0;
-    for (s1_idx=0; s1_idx<s1_len+1; s1_idx++) {
-        v0[s1_idx] = s1_idx;
-    }
-    var char_s1='', char_s2='';
-    for (s2_idx=1; s2_idx<=s2_len; s2_idx++) {
-        v1[0] = s2_idx;
-        char_s2 = s2[s2_idx - 1];
+	var s1_idx=0, s2_idx=0, cost=0;
+	for (s1_idx=0; s1_idx<s1_len+1; s1_idx++) {
+		v0[s1_idx] = s1_idx;
+	}
+	var char_s1='', char_s2='';
+	for (s2_idx=1; s2_idx<=s2_len; s2_idx++) {
+		v1[0] = s2_idx;
+		char_s2 = s2[s2_idx - 1];
 
-        for (s1_idx=0; s1_idx<s1_len;s1_idx++) {
-            char_s1 = s1[s1_idx];
-            cost = (char_s1 == char_s2) ? 0 : 1;
-            var m_min = v0[s1_idx+1] + 1;
-            var b = v1[s1_idx] + 1;
-            var c = v0[s1_idx] + cost;
-            if (b < m_min) {
-                m_min = b; }
-            if (c < m_min) {
-                m_min = c; }
-            v1[s1_idx+1] = m_min;
-        }
-        var v_tmp = v0;
-        v0 = v1;
-        v1 = v_tmp;
-    }
-    return v0[s1_len];
+		for (s1_idx=0; s1_idx<s1_len;s1_idx++) {
+			char_s1 = s1[s1_idx];
+			cost = (char_s1 == char_s2) ? 0 : 1;
+			var m_min = v0[s1_idx+1] + 1;
+			var b = v1[s1_idx] + 1;
+			var c = v0[s1_idx] + cost;
+			if (b < m_min) {
+				m_min = b; }
+			if (c < m_min) {
+				m_min = c; }
+			v1[s1_idx+1] = m_min;
+		}
+		var v_tmp = v0;
+		v0 = v1;
+		v1 = v_tmp;
+	}
+	return v0[s1_len];
 }
 
 // qs_score - Quicksilver Score
@@ -224,63 +230,63 @@ String.prototype.score = function(abbreviation,offset) {
   if(abbreviation.length > this.length) return 0.0
 
   for (var i = abbreviation.length; i > 0; i--) {
-    var sub_abbreviation = abbreviation.substring(0,i)
-    var index = this.indexOf(sub_abbreviation)
+	var sub_abbreviation = abbreviation.substring(0,i)
+	var index = this.indexOf(sub_abbreviation)
 
 
-    if(index < 0) continue;
-    if(index + abbreviation.length > this.length + offset) continue;
+	if(index < 0) continue;
+	if(index + abbreviation.length > this.length + offset) continue;
 
-    var next_string       = this.substring(index+sub_abbreviation.length)
-    var next_abbreviation = null
+	var next_string		  = this.substring(index+sub_abbreviation.length)
+	var next_abbreviation = null
 
-    if(i >= abbreviation.length)
-      next_abbreviation = ''
-    else
-      next_abbreviation = abbreviation.substring(i)
+	if(i >= abbreviation.length)
+	  next_abbreviation = ''
+	else
+	  next_abbreviation = abbreviation.substring(i)
  
-    var remaining_score   = next_string.score(next_abbreviation,offset+index)
+	var remaining_score   = next_string.score(next_abbreviation,offset+index)
  
-    if (remaining_score > 0) {
-      var score = this.length-next_string.length;
+	if (remaining_score > 0) {
+	  var score = this.length-next_string.length;
 
-      if(index != 0) {
-        var j = 0;
+	  if(index != 0) {
+		var j = 0;
 
-        var c = this.charCodeAt(index-1)
-        if(c==32 || c == 9) {
-          for(var j=(index-2); j >= 0; j--) {
-            c = this.charCodeAt(j)
-            score -= ((c == 32 || c == 9) ? 1 : 0.15)
-          }
+		var c = this.charCodeAt(index-1)
+		if(c==32 || c == 9) {
+		  for(var j=(index-2); j >= 0; j--) {
+			c = this.charCodeAt(j)
+			score -= ((c == 32 || c == 9) ? 1 : 0.15)
+		  }
 
-          // XXX maybe not port this heuristic
-          // 
-          //          } else if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location]]) {
-          //            for (j = matchedRange.location-1; j >= (int) searchRange.location; j--) {
-          //              if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:j]])
-          //                score--;
-          //              else
-          //                score -= 0.15;
-          //            }
-        } else {
-          score -= index
-        }
-      }
+		  // XXX maybe not port this heuristic
+		  // 
+		  //		  } else if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location]]) {
+		  //			for (j = matchedRange.location-1; j >= (int) searchRange.location; j--) {
+		  //			  if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:j]])
+		  //				score--;
+		  //			  else
+		  //				score -= 0.15;
+		  //			}
+		} else {
+		  score -= index
+		}
+	  }
    
-      score += remaining_score * next_string.length
-      score /= this.length;
-      return score
-    }
+	  score += remaining_score * next_string.length
+	  score /= this.length;
+	  return score
+	}
   }
   return 0.0
 }
 
 // http://stackoverflow.com/questions/967096/using-jquery-to-test-if-an-input-has-focus
 jQuery.extend(jQuery.expr[':'], {
-    focus: function(element) { 
-        return element == document.activeElement; 
-    }
+	focus: function(element) { 
+		return element == document.activeElement; 
+	}
 });
 
 // Create a responsive design menu.
