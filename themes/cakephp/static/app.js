bfbfc6ee7e83c4e8e53a835ed5fa2af2472feca7
Mark Story <mark@mark-story.com>
2016-05-02 23:16:20 -0400

    Pull in the JS from the design mocks.

--- themes/cakephp/static/app.js
+++ themes/cakephp/static/app.js
@@ -235,5 +235,175 @@ $.fn.menuSelect = function (options) {
 };
 } (jQuery));
 
+
+(function($){
+	"use strict";
+/* ********************* Helper functions ********************* */
+
+	/* Validate function */
+	function validate(data, def) {
+		return (data !== undefined) ? data : def;
+	}
+
+	var $win = $(window),
+
+		$body = $('body'),
+
+		// Window width without scrollbar
+		$windowWidth = $win.width(),
+
+		// Media Query fix (outerWidth -- scrollbar)
+		// Media queries width include the scrollbar
+		mqWidth = $win.outerWidth(true, true),
+
+		// Detect Mobile Devices 
+		isMobileDevice = (( navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone|IEMobile|Opera Mini|Mobi/i) || (mqWidth < 767) ) ? true : false );
+
+		// detect IE browsers
+		var ie = (function(){
+			var rv = 0,
+				ua = window.navigator.userAgent,
+				msie = ua.indexOf('MSIE '),
+				trident = ua.indexOf('Trident/');
+
+			if (msie > 0) {
+					// IE 10 or older => return version number
+					rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+			} else if (trident > 0) {
+					// IE 11 (or newer) => return version number
+					var rvNum = ua.indexOf('rv:');
+					rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
+			}
+
+			return ((rv > 0) ? rv : 0);
+		}());
+
+/* ********************* Megamenu ********************* */
+	var menu = $(".menu"),
+		Megamenu = {
+			desktopMenu: function() {
+
+				menu.children("li").show(0);
+				menu.children(".toggle-menu").hide(0);
+
+				// Mobile touch for tablets > 768px
+				if (isMobileDevice) {
+					menu.on("click touchstart","a", function(e){
+						if ($(this).attr('href') === '#') {
+							e.preventDefault();
+							e.stopPropagation();
+						}
+
+						var $this = $(this),
+							$sub = $this.siblings(".submenu, .megamenu");
+
+						$this.parent("li").siblings("li").find(".submenu, .megamenu").stop(true, true).fadeOut(300);
+
+						if ($sub.css("display") === "none") {
+							$sub.stop(true, true).fadeIn(300);
+						} else {
+							$sub.stop(true, true).fadeOut(300);
+							$this.siblings(".submenu").find(".submenu").stop(true, true).fadeOut(300);
+						}
+					});
+
+					$(document).on("click.menu touchstart.menu", function(e){
+						if ($(e.target).closest(menu).length === 0) {
+							menu.find(".submenu, .megamenu").fadeOut(300);
+						}
+					});
+					
+				// Desktop hover effect
+				} else {
+					menu.find('li').on({
+						"mouseenter": function() {
+							$(this).children(".submenu, .megamenu").stop(true, true).fadeIn(300);
+						},
+						"mouseleave": function() {
+							$(this).children(".submenu, .megamenu").stop(true, true).fadeOut(300);
+						}
+					});
+				}
+			},
+
+			mobileMenu: function() {
+				var $children = menu.children("li"),
+					$toggle = menu.children("li.toggle-menu"),
+					$notToggle = $children.not("toggle-menu");
+
+				$notToggle.hide(0);
+				$toggle.show(0).on("click", function(){
+
+					if ($children.is(":hidden")){
+						$children.slideDown(300);
+					} else {
+						$notToggle.slideUp(300);
+						$toggle.show(0);
+					}
+				});
+
+				// Click (touch) effect
+				menu.find("li").not(".toggle-menu").each(function(){
+					var $this = $(this);
+					if ($this.children(".submenu, .megamenu").length) {
+						$this.children("a").on("click", function(e){
+							if ($(this).attr('href') === '#') {
+								e.preventDefault();
+								e.stopPropagation();
+							}
+
+							var $sub = $(this).siblings(".submenu, .megamenu");
+
+							if ($sub.hasClass("open")) {
+								$sub.slideUp(300).removeClass("open");
+							} else {
+								$sub.slideDown(300).addClass("open");
+							}
+						});
+					}
+				});
+			},
+			unbindEvents: function() {
+				menu.find("li, a").off();
+				$(document).off("click.menu touchstart.menu");
+				menu.find(".submenu, .megamenu").hide(0);
+			}
+		}; // END Megamenu object
+
+	if ($windowWidth < 768) {
+		Megamenu.mobileMenu();
+	} else {
+		Megamenu.desktopMenu();
+	}
+
+/* ********************* Vertical / Fullscreen Menu ********************* */
+	// Vertical / Fullscreen Menu Trigger
+	$('#menu-trigger').on("click",function() {
+
+		if ($(this).hasClass('fullscreen-trigger')) {
+			$(".fullscreen-menu-wrapper").toggleClass("on");
+		} else if ($(this).hasClass("top-menu-trigger")) {
+			$(".top-menu-wrapper").toggleClass("on");
+		} else {
+			$(".vertical-menu-wrapper").toggleClass("on");
+			$(".vertical-menu-footer").toggleClass("on");
+		}
+
+		$(this).toggleClass("menu-close");
+		return false;
+	});
+
+/* ********************* Fixed Header ********************* */
+	function fixedHeader() {
+		$(".main-header").sticky({ 
+			topSpacing: 0,
+			className:"menu-fixed"
+		});
+	}
+	if ( (!$('.static-menu').length) && ($windowWidth > 991) && (!isMobileDevice) ) {
+		fixedHeader();
+	}
+});
+
 $(document).ready(App.Book.init);
 $(document).ready(App.InlineSearch.init);
