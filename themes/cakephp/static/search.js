d7994b99315c5c5a05e00fe74f0e2cd1c01712be
Mark Story <mark@mark-story.com>
2016-05-28 11:40:16 +0200

    Update design in the 1.2 branch.

--- themes/cakephp/static/search.js
+++ themes/cakephp/static/search.js
@@ -1,67 +1,139 @@
 App = window.App || {};
 
 App.Search = (function () {
-	var searchInput,
-		searchResults,
-		doSearch,
-		limit = 25,
-		paginationContainer;
+  var searchInput,
+    base = App.InlineSearch.baseUrl,
+    searchResults,
+    limit = 25,
+    paginationContainer;
 
+    var urldecode = function(x) {
+      return decodeURIComponent(x).replace(/\+/g, ' ');
+    };
+/**
+ * This function returns the parsed url parameters of the
+ * current request. Multiple values per key are supported,
+ * it will always return arrays of strings for the value parts.
+ */
+  var getQueryParameters = function(s) {
+    if (typeof s == 'undefined') {
+      s = document.location.search;
+    }
+    var parts = s.substr(s.indexOf('?') + 1).split('&');
+    var result = {};
+    for (var i = 0; i < parts.length; i++) {
+      var tmp = parts[i].split('=', 2);
+      var key = urldecode(tmp[0]);
+      var value = urldecode(tmp[1]);
+      if (key in result) {
+        result[key].push(value);
+      } else {
+        result[key] = [value];
+      }
+    }
+    return result;
+  };
 
-	var handleKeyEvent = function (event) {
-		App.InlineSearch.delay(function () {
-			searchResults.empty();
-			doSearch(searchInput.val());
-		}, 200);
-	};
+  function executeSearch(value, page) {
+    var query = {lang: window.lang, q: value, version: App.config.version};
+    if (page) {
+      query.page = page;
+    }
+    var url = App.config.url + '?' + jQuery.param(query);
+    var xhr = $.ajax({
+      url: url,
+      dataType: 'json',
+      type: 'GET'
+    });
 
-	var paginate = function (event) {
-		var active = $(event.target);
-		var page = active.attr('page');
-		event.preventDefault();
+    xhr.done(function (response) {
+      var results;
+      searchResults.empty().append('<ul></ul>');
+      results = response.data.slice(0, limit);
+      createResults(results);
+      createPagination(response.total, response.page);
+    });
+  }
 
-		App.InlineSearch.executeSearch(searchInput.val(), searchResults, undefined, page);
-	};
+  var delay = (function () {
+    var timer;
+    return function delay(callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    }
+  }());
 
-	var createPagination = function (event, results) {
-		var element, i;
-		var total = results.total;
-		var page = results.page;
-		var pages = Math.floor(total / limit);
+  /**
+   * Generate the result HTML.
+   */
+  function createResults(results) {
+      var ul = searchResults.find('ul');
+      $.each(results, function(index, item) {
+        var li = $('<li></li>');
+        var link = $('<a></a>');
+        link.attr('href', base + item.url);
+        if (item.title) {
+          link.append('<strong>' + item.title + '</strong><br />');
+        }
+        var span = $('<span></span>');
+        span.text(item.contents.join("\n"));
+        link.append(span);
+        li.append(link);
+        ul.append(li);
+      });
+  }
 
-		paginationContainer.empty();
+  /**
+   * Create the pagination links for a result set.
+   */
+  function createPagination(total, page) {
+    var element, i;
+    var pages = Math.floor(total / limit);
 
-		for (i = 1; i <= pages; i++) {
-			element = $('<a href="#"></a>');
-			element.text(i);
-			element.attr('page', i);
-			if (i == page) {
-				element.addClass('active');
-			}
-			paginationContainer.append(element);
-		}
-	};
+    paginationContainer.empty();
 
-	var init = function () {
-		searchInput = $('.standalone-search .search-input');
-		searchResults = $('#search-results');
-		paginationContainer = $('#search-pagination');
+    for (i = 1; i <= pages; i++) {
+      element = $('<a href="#"></a>');
+      element.text(i);
+      element.attr('page', i);
+      if (i == page) {
+        element.addClass('active');
+      }
+      paginationContainer.append(element);
+    }
+  };
 
-		searchInput.bind('keyup', handleKeyEvent);
-		$(document).bind('search.complete', createPagination);
-		paginationContainer.delegate('a', 'click', paginate);
+  var init = function () {
+    searchInput = $('.standalone-search .search-input');
+    searchResults = $('#search-results');
+    paginationContainer = $('#search-pagination');
 
-		doSearch = App.InlineSearch.createSearch(searchResults);
-		var params = $.getQueryParameters();
-		if (params.q) {
-			searchInput.val(params.q).trigger('keyup');
-		}
-	};
+    // Handle key up on the input box.
+    searchInput.bind('keyup', function(event) {
+      delay(function () {
+        searchResults.empty();
+        executeSearch(searchInput.val(), 1);
+      }, 200);
+    });
 
+    // Handle clickin on pagintion links
+    paginationContainer.delegate('a', 'click', function (event) {
+      var active = $(event.target);
+      var page = active.attr('page');
+      event.preventDefault();
 
-	return {
-		init: init
-	};
+      executeSearch(searchInput.val(), page);
+    });
+
+    var params = getQueryParameters();
+    if (params.q) {
+      searchInput.val(params.q).trigger('keyup');
+    }
+  };
+
+  return {
+    init: init
+  };
 })();
 
-$(App.Search.init);
+$(document).ready(App.Search.init);
