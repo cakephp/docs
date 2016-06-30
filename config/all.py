5acdbf1391d1150f7d97c7109c2328ec7791cc3e
cake17 <cake17@cake-websites.com>
2016-06-30 09:12:00 +0200

    change zh_CN to zh in config

--- config/all.py
+++ config/all.py
@@ -1,4 +1,4 @@
-# Global configuration information used across all the 
+# Global configuration information used across all the
 # translations of documentation.
 #
 
@@ -291,7 +291,7 @@ epub_tocdup = False
 epub_use_index = False
 
 # Languages available.
-languages = ['en', 'pt', 'es', 'ja', 'fr', 'zh_CN']
+languages = ['en', 'pt', 'es', 'ja', 'fr', 'zh']
 
 # The GitHub branch name for this version of the docs
 branch = 'master'
