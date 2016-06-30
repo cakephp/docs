69bd6081aef3b54f64ff2a163dc7622565e6825e
cake17 <cake17@cake-websites.com>
2016-06-30 09:36:21 +0200

    change copyright dates

--- config/all.py
+++ config/all.py
@@ -30,7 +30,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'CakePHP Cookbook'
-copyright = u'2014, Cake Software Foundation, Inc'
+copyright = u'2016, Cake Software Foundation, Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -244,7 +244,7 @@ man_pages = [
 epub_title = u'CakePHP Cookbook'
 epub_author = u'Cake Software Foundation, Inc.'
 epub_publisher = u'Cake Software Foundation, Inc.'
-epub_copyright = u'2013, Cake Software Foundation, Inc.'
+epub_copyright = u'2016, Cake Software Foundation, Inc.'
 
 epub_theme = 'cakephp-epub'
 
