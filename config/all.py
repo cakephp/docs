fae177f3934980f487392ef3e286e218b3410240
lilHermit <developer@lilhermit.co.uk>
2017-02-18 16:05:16 +0000

    Changes to depreciated config

--- config/all.py
+++ config/all.py
@@ -165,11 +165,23 @@ htmlhelp_basename = 'CakePHPCookbookdoc'
 
 # -- Options for LaTeX output ------------------------------------------------
 
-# The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-latex_font_size = '11pt'
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    'pointsize': '11pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author,
