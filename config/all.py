0cbe2c8431475e14600df5c2f25af35dce8d5fce
Koji Tanaka <16202+tenkoma@users.noreply.github.com>
2018-02-01 21:48:49 +0900

    Fix version select link for 1.3

--- config/all.py
+++ config/all.py
@@ -24,7 +24,7 @@ version_name = 'Red Velvet'
 version_list = [
     {'name': '3.x', 'number': '3.0', 'current': True, 'title': '3.x Book'},
     {'name': '2.x', 'number': '2.0', 'title': '2.x Book'},
-    {'name': '1.3', 'number': '3.0', 'title': '1.3 Book'},
+    {'name': '1.3', 'number': '1.3', 'title': '1.3 Book'},
     {'name': '1.2', 'number': '1.2', 'title': '1.2 Book'},
     {'name': '1.1', 'number': '1.1', 'title': '1.1 Book'},
 ]
