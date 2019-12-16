a96677633c466fa3bc52c010d7f17939cb5ee751
Mark Story <mark@mark-story.com>
2019-12-15 21:23:30 -0500

    Update version numbers

--- config/all.py
+++ config/all.py
@@ -9,10 +9,10 @@ from cakephpsphinx.config.all import *
 # built documents.
 #
 # The short X.Y version.
-version = '4.next'
+version = '4.x'
 
 # The full version, including alpha/beta/rc tags.
-release = '4.next'
+release = '4.x'
 
 # The search index version.
 search_version = '4-0'
@@ -22,7 +22,7 @@ version_name = 'Strawberry'
 
 # Other versions that display in the version picker menu.
 version_list = [
-    {'name': '4.0', 'number': '4', 'current': True, 'title': '4.x Book'},
+    {'name': '4.x', 'number': '4', 'current': True, 'title': '4.x Book'},
     {'name': '3.x', 'number': '3', 'title': '3.x Book'},
     {'name': '2.x', 'number': '2', 'title': '2.x Book'},
     {'name': '1.3', 'number': '1.3', 'title': '1.3 Book'},
