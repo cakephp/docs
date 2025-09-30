38a83810a585a609be45804fecc0a3b8e3db5c71
Mark Story <mark@mark-story.com>
2025-09-30 02:35:12 -0400

    Style note boxes more like current docs (#8097)
    * Style note boxes more like current docs
    
    - Switch the body font to Helvetica. Raleway isn't intended as a body
      copy face. We should continue using a Helvetica stack.
    - Import the icons and colors for tip/warning/note boxes to match the
      current book.
    
    * Use IBM plex as the body typeface

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -30,6 +30,7 @@ export default defineConfig({
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', { href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap', rel: 'stylesheet' }],
+    ['link', { href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap', rel: 'stylesheet'}],
   ],
   rewrites: {
     ':version/:slug*': ':version/:slug*'
