66dec70558b76b3023cda5140c4eee85d7b13144
Jasper Smet <josbeir@gmail.com>
2026-01-27 08:33:26 +0100

    Add sitemap.xml configuration

--- config.js
+++ config.js
@@ -23,6 +23,9 @@ export default {
   rewrites: {
     "en/:slug*": ":slug*",
   },
+  sitemap: {
+    hostname: "https://book.cakephp.org/5.next/",
+  },
   themeConfig: {
     socialLinks: [
       { icon: "github", link: "https://github.com/cakephp/cakephp" },
