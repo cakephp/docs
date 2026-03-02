de1659b36fb3f3bfa608f2cda9eeaf7323765d8c
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2026-03-02 15:23:24 +0100

    add dev version banner

--- config.js
+++ config.js
@@ -41,6 +41,11 @@ export default {
       { text: "Documentation", link: "/" },
       { ...versions },
     ],
+    versionBanner: {
+      message: 'This is a development version of documentation for CakePHP 6.0.',
+      link: '/latest/',
+      linkText: 'Go to latest docs.'
+    }
   },
   substitutions: {
     '|phpversion|': { value: '8.5', format: 'bold' },
